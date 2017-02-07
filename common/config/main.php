6137ecb6cd54a85ae5b3ebae5cc7d2127cec080b
gurguk <you@example.com>
2017-02-08 01:36:58 +0200

    graylog2

--- common/config/main.php
+++ common/config/main.php
@@ -12,5 +12,22 @@ return [
             ],
             '/tmp/'
         ),
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                'file' => [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+                'graylog' => [
+                    'class' => 'nex\graylog\GraylogTarget',
+                    'levels' => ['error', 'warning', 'info'],
+                    'categories' => ['application'],
+                    'logVars' => [],
+                    'host' => '127.0.0.1',
+                    'facility' => 'facility-name',
+                ],
+            ],
+        ],
     ],
 ];
